2597f02152cd224b628c475a786f62727490ec04
David ROBERT <davidrobertbcn@gmail.com>
2020-10-23 18:20:44 +0000

    update credit.c to the final version

--- credit.c
+++ credit.c
@@ -1,78 +1,138 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <stdbool.h>
-// #include <cs50.h>
+#include <string.h>
+#include <cs50.h>
 
-int main(void)
+bool valid(int sum)     // check if the sum is valid
 {
-    char card[20];
-    char *ptr;
-    long card2;
+
+    if (sum % 10 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void longToString(char card2[20], long cardNumber)    // long to string
+{
+    sprintf(card2, "%ld", cardNumber);
+}
+
+int lengthNumber(long cardNumber)   // check the length of the card number
+{
     int length;
 
-    printf("Number : ");
-    scanf("%s", &card);
-    
-    card2 = strtol(card, &ptr, 10);
-
-    //printf("%ld\n", card2);
-
-    for (length = 0; card2 > 0; length++)
+    for (length = 0; cardNumber > 0; length++)
     {
-        card2 /= 10;
+        cardNumber /= 10;
     }
 
-    //printf("%i\n", length);
+    return length;
+}
 
-    int multip = 0;
-    int rest = 0;
+int checkSum(int multip, int rest, int length, char card[20])   // checksum of the card number
+{
     int sum = 0;
 
     for (int i = length - 1; i >= 0; i--)
     {
-        if (i % 2 == 0)
+        if ((length - 1) % 2 != 0)
         {
-            multip = (card[i] - 48) * 2;
-
-            if (multip >= 10)
+            if (i % 2 == 0)
             {
-                rest = (multip % 10) + 1;
-                sum += rest;
+                multip = (card[i] - 48) * 2;
+
+                if (multip >= 10)
+                {
+                    rest = (multip % 10) + 1;
+                    sum += rest;
+                }
+                else
+                {
+                    sum += multip;
+                }
+
             }
             else
             {
-                sum += multip;
+                sum += (card[i] - 48);
             }
-            
         }
         else
         {
-            sum += (card[i] - 48);
+            if (i % 2 != 0)
+            {
+                multip = (card[i] - 48) * 2;
+
+                if (multip >= 10)
+                {
+                    rest = (multip % 10) + 1;
+                    sum += rest;
+                }
+                else
+                {
+                    sum += multip;
+                }
+
+            }
+            else
+            {
+                sum += (card[i] - 48);
+            }
         }
-        //printf("Somme0 : %i\n", sum);
-        
     }
 
-    bool valid;
+    return sum;
+}
 
-    if (sum % 10 == 0)
+
+int main(void)
+{
+    long cardNumber;
+    char card2[20];
+    int length;
+    int multip = 0, rest = 0;
+
+
+    do
     {
-        valid = 1;
+        cardNumber = get_long("Number : ");     // ask for a valid long card number
+    }
+    while (cardNumber < 1);
+
+    longToString(card2, cardNumber);    // pass the card number of long to string
+
+    length = lengthNumber(cardNumber);  // check the length of the card number
+
+    int sum = checkSum(multip, rest, length, card2);    // checksum of the card number
+
+    bool validSum = valid(sum);     // check if sum is valid
+
+    if (validSum)
+    {
+        if (length == 15 && card2[0] == 51 && (card2[1] == 52 || card2[1] == 55))   // AMEX check
+        {
+            printf("AMEX\n");
+        }
+        else if (length == 16 && card2[0] == 53 && (49 <= card2[1] && card2[1] <= 53))  // MASTERCARD check
+        {
+            printf("MASTERCARD\n");
+        }
+        else if ((length == 13 || length == 16) && card2[0] == 52)  // VISA check
+        {
+            printf("VISA\n");
+        }
+        else
+        {
+            printf("INVALID\n");
+        }
     }
     else
     {
-        valid = 0;
+        printf("INVALID\n");
     }
-
-    if (valid == 1 && card[0] == 52)
-    {
-        printf("VISA\n");
-    }
-    
-    
-    
-    
-    //printf("Somme : %i\n", sum);
 
 }
